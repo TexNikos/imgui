5bef8859eb9379ea9e00d01395166d55cfbf8bcd
TexNikos <texnikos.git@gmail.com>
2019-07-26 01:07:34 +0300

    Tabify premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -10,15 +10,15 @@ project "ImGui"
 	files
 	{
 		"imconfig.h",
-    "imgui.h",
-    "imgui.cpp",
-    "imgui_draw.cpp",
-    "imgui_internal.h",
-    "imgui_widgets.cpp",
-    "imstb_rectpack.h",
-    "imstb_textedit.h",
-    "imstb_truetype.h",
-    "imgui_demo.cpp"
+	    "imgui.h",
+	    "imgui.cpp",
+	    "imgui_draw.cpp",
+	    "imgui_internal.h",
+	    "imgui_widgets.cpp",
+	    "imstb_rectpack.h",
+	    "imstb_textedit.h",
+	    "imstb_truetype.h",
+	    "imgui_demo.cpp"
 	}
 	
 	filter "system:windows"
